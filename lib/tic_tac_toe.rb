b7bc147768766e225e500df0d2b9318adf02b3b1
Kameron Hungerford <kam.hungerford@gmail.com>
2020-08-02 21:52:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,7 +56,7 @@ class TicTacToe
   
   def turn
     puts "Pick a position between 1 and 9:"
-    input = gets.strip
+    input = gets.chomp
     index = input_to_index(input)
     player = current_player
     if valid_move?(index)
